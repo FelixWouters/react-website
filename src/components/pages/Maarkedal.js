01ea6487d7dd1a8620f2ab952fe3a633696d3d5a
Lunary1 <wouters.felix@gmail.com>
2021-08-26 18:55:03 +0200

    bootstrap added + carousel

--- src/components/pages/Maarkedal.js
+++ src/components/pages/Maarkedal.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import '../../App.css';
 import Portfolio from '../Portfolio';
+import Carousel from 'react-bootstrap/Carousel'
+import { CarouselItem } from 'react-bootstrap';
+import maarkedal from '../../assets/goethals/6.JPG'
+import maarkedal2 from '../../assets/goethals/Goethals-gevel3.jpeg'
+import maarkedal3 from '../../assets/goethals/Goethals-gevel7.jpeg'
+
 
 function Maarkedal() {
   return (
     <>
       <Portfolio />
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={maarkedal}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={maarkedal2}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={maarkedal3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 }
